cfb94f6695bbfe1d4a4ac3dd69e88fa4f7dffbbe
Rafał Gawlik <rafal.maly@gmail.com>
2015-08-02 16:22:16 +0200

    install react-select
    use @jniechcial fork so we can use custom option

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,3 +1,4 @@
 window.React = require('react');
 window.PropsListComponent = require('./components/props-list');
 window.SelectedUsersComponent = require('./components/selected-users');
+window.Select = require('react-select');
