b3b13003a8fa1219dad1491b327e713735ccc5f6
Rafał Gawlik <rafal.maly@gmail.com>
2015-09-09 16:13:29 +0200

    extract karma common, add karma-production.conf

--- karma-production.conf.js
+++ karma-production.conf.js
@@ -2,5 +2,7 @@ var webpack = require('webpack');
 var commonConfig = require('./webpack/karma-common.conf.js');
 
 module.exports = function (config) {
+  commonConfig.eslint.stopOnError = true;
+  commonConfig.eslint.stopOnWarning = true;
   config.set(commonConfig);
 };
---
+++ webpack/karma-common.conf.js
@@ -0,0 +1,36 @@
+module.exports = {
+  browsers: ['Chrome'],
+  singleRun: true,
+  frameworks: ['mocha', 'sinon'],
+  files: ['webpack/tests.config.js', {pattern: './app/react/**/*', included: false, served: false}],
+  preprocessors: {
+    'webpack/tests.config.js': ['webpack', 'sourcemap'],
+    'app/react/**/*.js*': ['eslint'],
+  },
+  reporters: ['dots'],
+  webpack: {
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }]
+    },
+    watch: true,
+    resolve: {
+      extensions: ["", ".js", ".jsx", ".js.jsx"]
+    },
+    devtool: 'inline-source-map',
+  },
+  webpackServer: {
+    noInfo: true
+  },
+  eslint: {
+    engine: {
+      configFile: 'app/react/.eslintrc'
+    },
+    stopOnError: false,
+    stopOnWarning: false
+  }
+}
