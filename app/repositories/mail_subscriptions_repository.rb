88ad6460c7979881cf0807712f6d07e160f5baf0
Adam Nowak <adam.nowak@netguru.co>
2015-09-20 21:27:25 +0200

    add additional methods to mail subscriptions repo

--- app/repositories/mail_subscriptions_repository.rb
+++ app/repositories/mail_subscriptions_repository.rb
@@ -1,10 +1,28 @@
 class MailSubscriptionsRepository
+  def all
+    MailSubscription.all
+  end
+
+  def weekly
+    active.where(interval: :weekly)
+      .where('last_sent_at >= ? OR last_sent_at IS NULL', 7.days.ago)
+  end
+
+  def daily
+    active.where(interval: :daily)
+      .where('last_sent_at >= ? OR last_sent_at IS NULL', 1.day.ago)
+  end
+
   def find_for_user(user)
-    MailSubscription.find_by(user_id: user.id)
+    all.find_by(user_id: user.id)
   end
 
   def update(subscription, attrs)
     subscription.assign_attributes(attrs)
     subscription.save
+  end
+
+  def active
+    all.where(active: true)
   end
 end
