0c94eb49d63b403e9712543484514493e2f9e6de
Adam Nowak <adam.nowak@netguru.co>
2015-09-20 22:20:01 +0200

    update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'props'
 set :repo_url,  'git://github.com/netguru/props.git'
 set :deploy_to, ENV['DEPLOY_PATH']
 
-set :linked_files, %w(config/database.yml config/sec_config.yml config/secrets.yml)
+set :linked_files, %w(config/database.yml config/secrets.yml)
 set :linked_dirs, %w(bin log tmp vendor/bundle)
 
 namespace :webpack do
