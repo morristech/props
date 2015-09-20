251f6dc81f9e005e561a672616cec7dc9da02392
Adam Nowak <adam.nowak@netguru.co>
2015-09-20 21:54:11 +0200

    deactivate mail subscription when user is archived

--- app/services/users/archive_user.rb
+++ app/services/users/archive_user.rb
@@ -9,6 +9,15 @@ module Users
     def call
       user.archived_at = Time.zone.now
       user.save!
+      remove_mail_subscription!
+    end
+
+    private
+
+    def remove_mail_subscription!
+      repo = MailSubscriptionsRepository.new
+      sub = repo.find_for_user(user)
+      sub.update_column(:active, false) if sub.present?
     end
   end
 end
