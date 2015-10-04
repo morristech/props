17c6dc17d926d14942b8851f6a0a3b944d1b653e
Rafał Gawlik <rafal.maly@gmail.com>
2015-10-04 16:04:59 +0200

    move and add more examples to small-thumb test

---
+++ app/react/components/user/thumb-small-test.jsx
@@ -0,0 +1,28 @@
+import React from 'react/addons';
+const TestUtils = React.addons.TestUtils;
+import expect from 'expect';
+import Thumb from './thumb-small';
+
+const userData = {
+  id: 1,
+  avatar_url: 'https://test1.img',
+  name: 'name',
+};
+
+describe('Users', () => {
+  const component = TestUtils.renderIntoDocument(<Thumb user={userData}/>);
+
+  it('renders', () => {
+    expect(TestUtils.findRenderedComponentWithType(component, Thumb)).toExist();
+  });
+
+  it('displays user avatar', () => {
+    const element = TestUtils.findRenderedDOMComponentWithTag(component, 'img').getDOMNode();
+    expect(element.getAttribute('src')).toEqual('https://test1.img');
+  });
+
+  it('links to user page', () => {
+    const element = TestUtils.findRenderedDOMComponentWithTag(component, 'a').getDOMNode();
+    expect(element.getAttribute('href')).toEqual('#users/1');
+  });
+});
