cd76cffd845c8a2af08f4bbba5f6c526eb3d6f8f
Jacek Jakubik <jacek.jakubik@netguru.pl>
2016-02-01 13:58:17 +0100

    Deployment code cleanup

--- Capfile
+++ Capfile
@@ -1,22 +1,7 @@
 require 'capistrano/setup'
 require 'capistrano/deploy'
 
-task :docker do
-  require 'capistrano/docker'
-  require 'capistrano/docker/npm'
-  require 'capistrano/docker/assets'
-  require 'capistrano/docker/migration'
-end
-
-task :non_docker do
-  require 'capistrano/bundler'
-  require 'capistrano/npm'
-  require 'capistrano/rails/assets'
-  require 'capistrano/rails/migrations'
-  require 'capistrano/passenger'
-  require 'rvm1/capistrano3'
-  require 'whenever/capistrano'
-end
-
-task 'production' => [:non_docker]
-task 'docker_production' => [:docker]
+require 'capistrano/docker'
+require 'capistrano/docker/npm'
+require 'capistrano/docker/assets'
+require 'capistrano/docker/migration'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,19 +1,30 @@
-require 'net/ssh/proxy/command'
 server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w(web app db)
-set :ssh_options, proxy: Net::SSH::Proxy::Command.new("ssh #{ENV['GATEWAY']} -W %h:%p")
 set :branch, 'production'
 
-set :linked_files, %w(config/database.yml config/secrets.yml)
-set :linked_dirs, %w(bin log tmp vendor/bundle node_modules)
+set :docker_volumes, [
+  "#{shared_path}/config/secrets.yml:/var/www/app/config/secrets.yml",
+  "#{shared_path}/log:/var/www/app/log",
 
-namespace :webpack do
-  task :build do
-    on roles :web do
-      within release_path do
-        execute :bundle, 'exec npm run build'
+  "props_production_node_modules:/var/www/app/node_modules",
+  "props_production_assets:/var/www/app/public/assets",
+  "#{shared_path}/assets/javascripts/react_bundle.js:/var/www/app/app/assets/javascripts/react_bundle.js",
+]
+
+set :docker_links, %w(postgres_ambassador:postgres)
+set :docker_additional_options, -> { "--env-file #{shared_path}/envfile" }
+set :docker_dockerfile, "docker/production/Dockerfile"
+
+set :docker_apparmor_profile, "docker-ptrace"
+set :docker_cpu_quota, "200000"
+
+namespace :docker do
+  namespace :npm do
+    task :build do
+      on roles(fetch(:docker_role)) do
+        execute :docker, task_command("npm run build")
       end
     end
   end
 end
 
-after 'npm:install', 'webpack:build'
+after "docker:npm:install", "docker:npm:build"
