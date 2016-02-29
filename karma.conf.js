5ede3bc886777f3ab69ecf9d1efb45e734860203
Radosław Piątek <radoslaw.piatek@netguru.pl>
2016-02-29 12:41:37 +0100

    Add karma notifier

--- karma.conf.js
+++ karma.conf.js
@@ -3,4 +3,9 @@ var commonConfig = require('./webpack/karma-common.conf.js');
 
 module.exports = function (config) {
   config.set(commonConfig);
+  config.reporters = ['progress', 'notify'];
+  config.notifyReporter = {
+    reportEachFailure: true, // Default: false, Will notify on every failed sepc
+    reportSuccess: true, // Default: true, Will notify when a suite was successful
+  };
 };
