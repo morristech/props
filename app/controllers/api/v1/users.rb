2316394aa3f0b556e18d0b80d4686040c15bd7bb
Marcin Jakubowski <marcin.jakubowski@netguru.pl>
2016-03-08 11:42:50 +0100

    add grape controller for users namespace

---
+++ app/controllers/api/v1/users.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class Users < Grape::API
+      include Api::V1::Defaults
+
+      helpers do
+        include Api::V1::Helpers
+
+        def users_repository
+          UsersRepository.new
+        end
+      end
+
+      before do
+        authenticate_user!
+      end
+
+      resources :users do
+        desc 'Return all active users'
+        get do
+          present users_repository.active, with: Entities::UserBase
+        end
+
+        namespace ':user_id' do
+          desc 'Return specific user'
+          params do
+            requires :user_id, type: Integer
+          end
+          get do
+            user = users_repository.find_by_id(params[:user_id])
+            present user, with: Entities::UserFull
+          end
+        end
+      end
+    end
+  end
+end
