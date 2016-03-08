aa94a77a8cc3fdb24463aec4b1f9ecf0b736012d
Marcin Jakubowski <marcin.jakubowski@netguru.pl>
2016-03-08 11:47:39 +0100

    add specs for rankings controller (grape)

---
+++ spec/requests/api/v1/rankings_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe Api::V1::Rankings do
+  let(:propser) { create(:user) }
+  let(:receiver) { create(:user) }
+  let(:prop_receiver) { PropReceiver.create(user: receiver) }
+  let!(:prop) { Prop.create(body: 'sample', propser: propser, prop_receivers: [prop_receiver]) }
+  let!(:api_token) { EasyTokens::Token.create(value: 'aaabbbccc', owner_id: propser.id) }
+
+  describe 'GET /api/v1/rankings/hero_of_the_week' do
+    before do
+      get '/api/v1/rankings/hero_of_the_week', token: token
+    end
+
+    context 'with invalid api token' do
+      let(:token) { 'cccbbbaaa' }
+
+      it "returns 'Invalid token' response" do
+        result = json_response['error']
+        expect(result).to eq 'Invalid token'
+      end
+    end
+
+    context 'with valid api token' do
+      let(:token) { api_token.value }
+
+      it 'returns user with the most received props' do
+        expect(json_response['user']).to eq receiver.name
+        expect(json_response['props_count']).to eq 1
+      end
+    end
+  end
+end
