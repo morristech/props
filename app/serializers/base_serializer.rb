de07a272df9743e7398b5b32a940b1ddff130c36
Marcin Jakubowski <marcin.jakubowski@netguru.pl>
2016-03-08 11:44:43 +0100

    remove unused active model serializers
    replaced with Grape::Entity

--- app/serializers/base_serializer.rb
+++
@@ -1,5 +0,0 @@
-class BaseSerializer < ActiveModel::Serializer
-  def gravatar_url(email)
-    Gravatar.new(email).image_url(secure: true)
-  end
-end
--- app/serializers/user_serializer/base.rb
+++
@@ -1,9 +0,0 @@
-module UserSerializer
-  class Base < BaseSerializer
-    attributes :id, :name, :email, :avatar_url
-
-    def avatar_url
-      gravatar_url(object.email)
-    end
-  end
-end
