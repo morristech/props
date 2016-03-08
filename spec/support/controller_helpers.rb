d705810c256c0152bf893e9603773507137fec22
Marcin Jakubowski <marcin.jakubowski@netguru.pl>
2016-03-08 11:46:22 +0100

    remove helpers for controller specs

--- spec/support/controller_helpers.rb
+++
@@ -1,9 +0,0 @@
-module ControllerHelpers
-  def sign_in(user = double('user'))
-    allow(controller).to receive(:current_user) { user }
-  end
-
-  def json_response
-    JSON.load(response.body)
-  end
-end
