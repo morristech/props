0357c6a5b5365b508d598c15dd604159cbddc583
Rafał Gawlik <rafal.maly@gmail.com>
2016-04-02 20:43:52 +0200

    validate prop receiver user on create - check if it's active

--- app/models/prop_receiver.rb
+++ app/models/prop_receiver.rb
@@ -3,4 +3,12 @@ class PropReceiver < ActiveRecord::Base
   belongs_to :user
 
   validates :prop, :user, presence: true
+  validate :active_user?, on: :create
+
+  private
+
+  def active_user?
+    return if user.archived_at.nil?
+    errors.add(:user_id, I18n.t('props.errors.archived_user', name: user.name))
+  end
 end
