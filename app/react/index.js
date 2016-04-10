4764d0f7e93bd0cca2daa7d9e0a690e1122d98bb
Rafał Gawlik <rafal.maly@gmail.com>
2016-04-10 22:53:00 +0200

    add dummy app component

--- app/react/index.js
+++ app/react/index.js
@@ -1,20 +1,5 @@
 import RWR from 'react-webpack-rails';
 RWR.run();
 
-import PropsListComponent from './components/props';
-import SelectedUsersComponent from './components/selected-users';
-import Select from 'react-select';
-import UserOptionComponent from './components/prop-user-option';
-import UserThumb from './components/user/thumb';
-import AnnouncementComponent from './components/announcement';
-import NavbarComponent from './components/navbar/navbar';
-import ReduxContainer from './components/redux-container';
-
-RWR.registerComponent('PropsListComponent', PropsListComponent);
-RWR.registerComponent('SelectedUsersComponent', SelectedUsersComponent);
-RWR.registerComponent('Select', Select);
-RWR.registerComponent('UserOptionComponent', UserOptionComponent);
-RWR.registerComponent('UserThumb', UserThumb);
-RWR.registerComponent('AnnouncementComponent', AnnouncementComponent);
-RWR.registerComponent('NavbarComponent', NavbarComponent);
-RWR.registerComponent('ReduxContainer', ReduxContainer);
+import App from './components/app';
+RWR.registerComponent('App', App);
