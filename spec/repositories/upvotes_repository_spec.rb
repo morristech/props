23d209c8354512858d7c8a0edd81d132865328ef
Sebastian Brych <sebastian+github@nwscx.com>
2016-04-28 16:21:12 +0200

    Upvotes repository update

--- spec/repositories/upvotes_repository_spec.rb
+++ spec/repositories/upvotes_repository_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe UpvotesRepository do
   let(:repo) { described_class.new }
   let(:user) { create(:user) }
+  let(:user2) { create(:user) }
   let(:prop) { create(:prop) }
 
   describe '#add' do
@@ -16,6 +17,30 @@ describe UpvotesRepository do
       expect do
         repo.add(prop, user)
       end.to change { prop.reload.upvotes_count }.by(1)
+    end
+  end
+
+  describe '#remove' do
+    before { repo.add(prop, user) }
+
+    context 'user upvoted the prop' do
+      it 'removes an upvote' do
+        expect{ repo.remove(prop, user) }.to change{ Upvote.count }.by(-1)
+      end
+
+      it "decrements upvotes_count on props" do
+        expect { repo.remove(prop, user) }.to change { prop.reload.upvotes_count }.by(-1)
+      end
+    end
+
+    context 'different user upvoted the prop' do
+      it 'removes an upvote' do
+        expect{ repo.remove(prop, user2) }.to change{ Upvote.count }.by(0)
+      end
+
+      it "decrements upvotes_count on props" do
+        expect { repo.remove(prop, user2) }.to change { prop.reload.upvotes_count }.by(0)
+      end
     end
   end
 end
