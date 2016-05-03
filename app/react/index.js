c12b5e0cb3b879edbed7e548c0659be060c3afa5
Radosław Piątek <radoslaw.piatek@netguru.pl>
2016-05-03 22:57:06 +0200

    Move props related components to sub directory
    I did not care about resolve paths for backbone

--- app/react/index.js
+++ app/react/index.js
@@ -3,18 +3,12 @@ RWR.run();
 
 import AppContainer from './components/app-container';
 import PropsListComponent from './components/props';
-import SelectedUsersComponent from './components/selected-users';
-import Select from 'react-select';
-import UserOptionComponent from './components/prop-user-option';
 import UserThumb from './components/user/thumb';
 import AnnouncementComponent from './components/announcement';
 import NavbarComponent from './components/navbar/navbar';
 
 RWR.registerComponent('AppContainer', AppContainer);
 RWR.registerComponent('PropsListComponent', PropsListComponent);
-RWR.registerComponent('SelectedUsersComponent', SelectedUsersComponent);
-RWR.registerComponent('Select', Select);
-RWR.registerComponent('UserOptionComponent', UserOptionComponent);
 RWR.registerComponent('UserThumb', UserThumb);
 RWR.registerComponent('AnnouncementComponent', AnnouncementComponent);
 RWR.registerComponent('NavbarComponent', NavbarComponent);
