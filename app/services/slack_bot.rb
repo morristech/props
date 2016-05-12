0ecdfa5a786c9976a4360d28e679a46c5429f331
mejcz <magda.wisniewska@netguru.co>
2016-05-12 14:14:06 +0200

    Add listeners for reactions to SlackBot, update specs

--- app/services/slack_bot.rb
+++ app/services/slack_bot.rb
@@ -2,6 +2,7 @@ class SlackBot
   def initialize
     @client = Slack::RealTime::Client.new
     set_lifecycle_listeners
+    set_reaction_listeners
   end
 
   def listen
@@ -10,21 +11,45 @@ class SlackBot
 
   private
 
+  def reaction(data)
+    Reaction.new data[:reaction],
+                 data[:item][:ts],
+                 user_profile(data[:user])
+  end
+
+  def reaction_added(data)
+    puts reaction(data).upvote
+  end
+
+  def reaction_removed(data)
+    puts reaction(data).undo_upvote
+  end
+
   def hello
     puts "Connected to '#{@client.team.name}' team at https://#{@client.team.domain}.slack.com."
   end
 
   def close
-    puts "SlackBot is about to disconnect"
+    puts 'SlackBot is about to disconnect'
   end
 
   def closed
-    puts "SlackBot has disconnected successfully!"
+    puts 'SlackBot has disconnected successfully!'
+  end
+
+  def set_reaction_listeners
+    %i(reaction_added reaction_removed).each do |event|
+      @client.on(event) { |data| send(event, data) }
+    end
   end
 
   def set_lifecycle_listeners
     %i(hello close closed).each do |event|
       @client.on(event) { send(event) }
     end
+  end
+
+  def user_profile(id)
+    @client.web_client.users_list[:members].detect { |member| member[:id] == id }
   end
 end
