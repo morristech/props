1fe0e8d098d8cd9c9ac5080dcbba0fe2820b7759
mejcz <magda.wisniewska@netguru.co>
2016-05-13 15:23:53 +0200

    Resolve issues with spaces around brackets

--- spec/services/slack_notifier_spec.rb
+++ spec/services/slack_notifier_spec.rb
@@ -4,22 +4,22 @@ describe Notifier::SlackNotifier do
   let(:prop) { create(:prop) }
   let(:notification) { NewPropNotification.new(prop) }
 
-  subject { described_class.new(notification)}
+  subject { described_class.new(notification) }
 
   describe '#call' do
     context 'slack bot is configured properly' do
       before do
-        allow(subject.send(:channel)).to receive(:chat_postMessage).and_return({ ts: 'test_ts' })
+        allow(subject.send(:channel)).to receive(:chat_postMessage).and_return(ts: 'test_ts')
       end
 
       it 'should update slack_ts attribute' do
-        expect{ subject.call }.to change{ prop.slack_ts }.to('test_ts')
+        expect { subject.call }.to change(prop, :slack_ts).to('test_ts')
       end
     end
 
     context 'slack bot is not configured' do
       it 'should not update slack_ts' do
-        expect{ subject.call }.to_not change{ prop.slack_ts }
+        expect { subject.call }.to_not change(prop, :slack_ts)
       end
     end
   end
