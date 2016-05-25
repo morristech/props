b2352e7e9f102acbc1baf5089d88e417154c680a
mejcz <magda.wisniewska@netguru.co>
2016-05-25 16:15:43 +0200

    Create job for notifying about new props

---
+++ app/jobs/notifier_job.rb
@@ -0,0 +1,14 @@
+class NotifierJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(prop_id)
+    Notifier.new(notification(prop_id)).call
+  end
+
+  private
+
+  def notification(prop_id)
+    prop =  Prop.find(prop_id)
+    NewPropNotification.new prop
+  end
+end
---
+++ spec/jobs/notifier_job_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe NotifierJob, type: :job do
+  include ActiveJob::TestHelper
+
+  let(:prop) { create(:prop) }
+  let(:notifier) { double() }
+  subject { described_class.perform_later(prop.id) }
+
+  before do
+    allow(Notifier).to receive(:new) { notifier }
+  end
+
+  it 'adds job to queue' do
+    expect { subject }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by 1
+  end
+
+  it 'executes perform' do
+    expect(notifier).to receive(:call)
+    perform_enqueued_jobs { subject }
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+end
