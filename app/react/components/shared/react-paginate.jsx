755fc4dcee80b14a15a6f6b7b400702f93b70665
Michal Jezynski <michal.jezynski@netguru.co>
2016-06-15 17:49:30 +0200

    use constants

--- app/react/components/shared/react-paginate.jsx
+++ app/react/components/shared/react-paginate.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import ReactPaginate from 'react-paginate';
 
 export default class Pagination extends React.Component {
+
   static get propTypes() {
     const { func, number } = PropTypes;
     return {
@@ -13,23 +14,32 @@ export default class Pagination extends React.Component {
 
   render() {
     const { totalPages, currentPage, onPageChange } = this.props;
-
+    var forceSelected = currentPage - 1;
     return (
       <nav>
         <ReactPaginate
-          previousLabel={"previous"}
-          nextLabel={"next"}
-          breakLabel={<a href="">...</a>}
+          previousLabel={Pagination.previousLabel}
+          nextLabel={Pagination.nextLabel}
+          breakLabel={Pagination.breakLabel}
           pageNum={totalPages}
-          forceSelected={currentPage - 1}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
+          forceSelected={forceSelected}
+          marginPagesDisplayed={Pagination.marginPagesDisplayed}
+          pageRangeDisplayed={Pagination.pageRangeDisplayed}
           clickCallback={onPageChange}
-          containerClassName={"pagination"}
-          subContainerClassName={"pages pagination"}
-          activeClassName={"active"}
+          containerClassName={Pagination.containerClassName}
+          subContainerClassName={Pagination.subContainerClassName}
+          activeClassName={Pagination.activeClassName}
         />
       </nav>
     );
   }
 }
+
+Object.defineProperty(Pagination, 'previousLabel', {value: 'previous'});
+Object.defineProperty(Pagination, 'nextLabel', {value: 'next'});
+Object.defineProperty(Pagination, 'breakLabel', {value: <a href="">...</a>});
+Object.defineProperty(Pagination, 'marginPagesDisplayed', {value: 2});
+Object.defineProperty(Pagination, 'pageRangeDisplayed', {value: 5});
+Object.defineProperty(Pagination, 'containerClassName', {value: 'pagination'});
+Object.defineProperty(Pagination, 'subContainerClassName', {value: "pages pagination"});
+Object.defineProperty(Pagination, 'activeClassName', {value: "active"});
