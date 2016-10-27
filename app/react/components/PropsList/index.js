1c1ff5bb368278463892d2392424ad125bab9699
Artur Andersz <artur.andersz@netguru.pl>
2016-10-27 13:02:26 +0200

    add simple component using react for render only

---
+++ app/react/components/PropsList/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PropsList extends Component {
+  render() {
+    return (
+      <div>Props List View</div>
+    )
+  }
+}
+
+PropsList.propTypes = {
+
+};
+
+export default PropsList;
---
+++ app/react/containers/PropsList/index.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import PropsList from '../../components/PropsList';
+
+
+const mapStateToProps = (state) => ({
+});
+
+const mapDispatchToProps = (dispatch) => ({
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PropsList);
