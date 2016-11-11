021feb0eba5a841f786dbbf81afc0d99d56d8e32
Artur Andersz <artur.andersz@netguru.pl>
2016-11-11 13:22:46 +0100

    props validation and statless components

--- app/react/components/PropsList/Prop.jsx
+++ app/react/components/PropsList/Prop.jsx
@@ -1,43 +1,47 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import moment from 'moment';
 
 import UserComponent from '../UsersList/User';
 
-export default class Prop extends React.Component {
-  static get propTypes() {
-    return {
-      prop: React.PropTypes.object.isRequired,
-      voteComponent: React.PropTypes.element,
-    };
-  }
+const Prop = ({ prop, voteComponent }) => {
+  const createdAt = moment(prop.createdAt || prop.created_at).fromNow();
+  const receivers = prop.users.map(receiver =>
+    <UserComponent user={receiver} key={receiver.id} />
+  );
 
-  render() {
-    const createdAt = moment(this.props.prop.createdAt || this.props.prop.created_at).fromNow();
-    const receivers = this.props.prop.users.map(receiver =>
-      <UserComponent user={receiver} key={receiver.id} />
-    );
-
-    return (
-      <li className="row list-group-item props.prop-list-item">
-        <div className="col-xs-12 prop-users">
-          <UserComponent user={this.props.prop.propser} />
-          <i className="glyphicon glyphicon-chevron-right prop-to" />
-          {receivers}
-        </div>
-        <div className="col-xs-12 prop-content">
-          <p className="lead prop-body">
-            {this.props.prop.body}
-          </p>
-          <div className="row">
-            <div className="col-xs-12 prop-footer">
-              <div className="prop-create-at pull-left">
-                {createdAt}
-              </div>
-              {this.props.voteComponent}
+  return (
+    <li className="row list-group-item props.prop-list-item">
+      <div className="col-xs-12 prop-users">
+        <UserComponent user={prop.propser} />
+        <i className="glyphicon glyphicon-chevron-right prop-to" />
+        {receivers}
+      </div>
+      <div className="col-xs-12 prop-content">
+        <p className="lead prop-body">
+          {prop.body}
+        </p>
+        <div className="row">
+          <div className="col-xs-12 prop-footer">
+            <div className="prop-create-at pull-left">
+              {createdAt}
             </div>
+            {voteComponent}
           </div>
         </div>
-      </li>
-    );
-  }
-}
+      </div>
+    </li>
+  );
+};
+
+Prop.propTypes = {
+  prop: PropTypes.shape({
+    created_at: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    users: PropTypes.array.isRequired,
+    propser: PropTypes.object.isRequired,
+
+  }),
+  voteComponent: PropTypes.element,
+};
+
+export default Prop;
--- app/react/components/PropsList/index.jsx
+++ app/react/components/PropsList/index.jsx
@@ -1,53 +1,54 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import Pagination from '../Shared/Pagination';
 import Prop from './Prop';
 
-class PropsList extends Component {
-  render() {
-    const {
-      hasPrevPage,
-      hasNextPage,
-      onPaginationPrev,
-      onPaginationNext,
-      currentPage,
-    } = this.props;
+const PropsList = ({
+  propsList,
+  hasPrevPage,
+  hasNextPage,
+  onPaginationPrev,
+  onPaginationNext,
+  currentPage,
+}) => {
+  const handlePrevPageClick = (e) => {
+    e.preventDefault();
+    onPaginationPrev(currentPage - 1);
+  };
 
-    const handlePrevPageClick = (e) => {
-      e.preventDefault();
-      onPaginationPrev(currentPage - 1);
-    };
+  const handleNextPageClick = (e) => {
+    e.preventDefault();
+    onPaginationNext(currentPage + 1);
+  };
 
-    const handleNextPageClick = (e) => {
-      e.preventDefault();
-      onPaginationNext(currentPage + 1);
-    };
-
-    return (
-      <div>
-        {
-          this.props.propsList.map(prop =>
-            <div>
-              <Prop
-                prop={prop}
-                voteComponent={null}
-              />
-            </div>
-          )
-        }
-        <Pagination
-          hasPreviousPage={hasPrevPage}
-          hasNextPage={hasNextPage}
-          onPrevPageClick={handlePrevPageClick}
-          onNextPageClick={handleNextPageClick}
-          currentPage={currentPage}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {
+        propsList.map(prop =>
+          <Prop
+            key={prop.id}
+            prop={prop}
+            voteComponent={null}
+          />
+        )
+      }
+      <Pagination
+        hasPreviousPage={hasPrevPage}
+        hasNextPage={hasNextPage}
+        onPrevPageClick={handlePrevPageClick}
+        onNextPageClick={handleNextPageClick}
+        currentPage={currentPage}
+      />
+    </div>
+  );
+};
 
 PropsList.propTypes = {
-
+  propsList: PropTypes.arrayOf(PropTypes.object),
+  hasPrevPage: PropTypes.bool.isRequired,
+  hasNextPage: PropTypes.bool.isRequired,
+  onPaginationPrev: PropTypes.func.isRequired,
+  onPaginationNext: PropTypes.func.isRequired,
+  currentPage: PropTypes.number.isRequired,
 };
 
 export default PropsList;
