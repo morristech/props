3e49c990a7a87f8a46c91f65e4b25bdb8731722a
Artur Andersz <artur.andersz@netguru.pl>
2017-02-09 01:31:17 +0100

    add multiselect
    prop-8

--- app/react/components/PropsList/AddProp.jsx
+++ app/react/components/PropsList/AddProp.jsx
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 import cx from 'classnames';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import { forIn } from 'lodash';
 import styles from './style.css';
 
 class AddProp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: [],
+    };
+
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  getOptions() {
+    const options = [];
+    const data = this.props.users;
+    forIn(data, (value) => {
+      options.push({ label: value.name, value: value.id });
+    });
+    return options;
+  }
+
+  handleSelectChange(value) {
+    console.log('You\'ve selected:', value);
+    this.setState({ value });
+  }
+
   render() {
     return (
       <div className="row">
@@ -13,11 +39,21 @@ class AddProp extends Component {
               styles.grid,
             )}
           >
-            Form
+            <form>
+              <Select
+                multi
+                disabled={false}
+                value={this.state.value}
+                placeholder="Whom do you want to give a prop to?"
+                options={this.getOptions()}
+                onChange={this.handleSelectChange}
+              />
+            </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
+
 export default AddProp;
