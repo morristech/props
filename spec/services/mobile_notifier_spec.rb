7c273840c00e6ed06ded9ec8e81ee9bf3171829c
Artur Figiel <arturffigiel@gmail.com>
2017-02-13 19:23:54 +0100

    Add integration tests for mobile notif service

--- spec/services/mobile_notifier_spec.rb
+++ spec/services/mobile_notifier_spec.rb
@@ -5,21 +5,47 @@ describe Notifier::MobileNotifier do
   # let(:user_with_pid) { create(:user, pid: create_test_user) }
   let(:user) { create(:user, :with_wrong_pid) }
 
-  describe '#call' do
-    # context 'user with proper player id' do
-    #   let(:prop) { create(:prop, users: [user_with_pid]) }
-    #   let(:notification) { NewPropNotification.new(prop) }
+  describe '#initialize' do
+    let(:notifier) { double }
+    subject { described_class.new(notifier) }
 
-    #   subject { described_class.new(notification).call }
-    #   it { is_expected.to eq '200 OK' }
-    # end
+    let(:onesignal_api_key) { double }
+    let(:onesignal_user_auth_key) { double }
 
-    context 'user with wrong player id' do
-      let(:prop) { create(:prop, users: [user]) }
-      let(:notification) { NewPropNotification.new(prop) }
+    before do
+      allow(Dotenv).to receive(:load).once
+      allow(ENV).to receive(:[]).with('ONESIGNAL_API_KEY').and_return(onesignal_api_key)
+      allow(ENV).to receive(:[]).with('ONESIGNAL_USER_AUTH_KEY').and_return(onesignal_user_auth_key)
+    end
 
-      subject { described_class.new(notification).call }
-      it { is_expected.to eq '400' }
+    it '' do
+      expect(Dotenv).to receive(:load).with(no_args).once.ordered
+      expect(OneSignal::OneSignal).to receive(:api_key=).with(onesignal_api_key).once.ordered
+      expect(OneSignal::OneSignal).to receive(:user_auth_key=).with(onesignal_user_auth_key)
+                                                              .once.ordered
+      subject
     end
   end
+
+  # describe '#notify' do
+  #   # context 'user with proper player id' do
+  #   #   let(:prop) { create(:prop, users: [user_with_pid]) }
+  #   #   let(:notification) { NewPropNotification.new(prop) }
+
+  #   #   subject { described_class.new(notification).call }
+  #   #   it { is_expected.to eq '200 OK' }
+  #   # end
+
+  #   context 'user with wrong player id' do
+  #     let(:prop) { create(:prop, users: [user]) }
+  #     let(:notification) { NewPropNotification.new(prop) }
+
+  #     subject { described_class.new(notification).call }
+  #     it { is_expected.to eq '400' }
+  #   end
+  # end
+
+  # describe '#send_notification' do
+
+  # end
 end
