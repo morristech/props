01870b732710cbdc95dd47f775489525046dcf4a
Artur Figiel <arturffigiel@gmail.com>
2017-02-21 14:23:52 +0100

    Add data for params checking

--- app/controllers/api/v1/sessions.rb
+++ app/controllers/api/v1/sessions.rb
@@ -13,7 +13,8 @@ module Api
           requires :uid, type: String, desc: 'User Google id'
         end
         get do
-          user = User.find_by(uid: params.uid)
+          data = declared(params)
+          user = User.find_by(uid: data.uid)
           error!({ error: 'User not found' }, 404) if user.nil?
           present user, with: Entities::UserBase
         end
@@ -29,7 +30,8 @@ module Api
         end
 
         post do
-          user = User.create_with_omniauth(params)
+          data = declared(params)
+          user = User.create_with_omniauth(data)
           present user, with: Entities::UserBase
         end
       end
