5ed8c9a9683204a0dc56a2838d23d14584076c39
Artur Andersz <artur.andersz@netguru.pl>
2017-03-30 21:00:00 +0200

    simplify syntax
    PROP-11

--- app/react/components/UsersList/index.jsx
+++ app/react/components/UsersList/index.jsx
@@ -1,35 +1,29 @@
 import React, { PropTypes } from 'react';
 import Thumb from './Thumb';
 
-const UsersList = ({ users, filterUsers, goToUserProfile }) => {
-  const handleQuery = (e) => {
-    filterUsers(e.target.value);
-  };
-
-  return (
-    <div>
-      <div className="page-header">
-        <h1>Users</h1>
-        <form>
-          <input type="text" onChange={handleQuery} />
-        </form>
-      </div>
-      <div className="row">
-        {
-          users.map(u => (
-            <Thumb
-              key={u.id}
-              id={u.id}
-              name={u.name}
-              avatarUrl={u.avatar_url}
-              handleClick={goToUserProfile}
-            />
-          ))
-        }
-      </div>
+const UsersList = ({ users, filterUsers, goToUserProfile }) => (
+  <div>
+    <div className="page-header">
+      <h1>Users</h1>
+      <form>
+        <input type="text" onChange={e => filterUsers(e.target.value)} />
+      </form>
     </div>
-  );
-};
+    <div className="row">
+      {
+        users.map(u => (
+          <Thumb
+            key={u.id}
+            id={u.id}
+            name={u.name}
+            avatarUrl={u.avatar_url}
+            handleClick={goToUserProfile}
+          />
+        ))
+      }
+    </div>
+  </div>
+);
 
 UsersList.propTypes = {
   users: PropTypes.arrayOf(PropTypes.shape({
