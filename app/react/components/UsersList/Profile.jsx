d58fb9bcb21f313b5d6a8f8c572436cf717b8817
Artur Andersz <artur.andersz@netguru.pl>
2017-04-14 14:06:08 +0200

    handle user change in profile
    PROP-13

--- app/react/components/UsersList/Profile.jsx
+++ app/react/components/UsersList/Profile.jsx
@@ -9,6 +9,13 @@ class Profile extends Component {
     this.props.getProfile(this.props.params.id);
   }
 
+  componentWillReceiveProps(nextProps) {
+    const nextId = nextProps.params.id;
+    if (this.props.params.id !== nextId) {
+      this.props.getProfile(nextId);
+    }
+  }
+
   render() {
     const {
       name,
@@ -39,6 +46,8 @@ Profile.propTypes = {
   userProfile: PropTypes.shape({
     name: PropTypes.string,
     archived: PropTypes.bool,
+    receivedProps: PropTypes.arrayOf(PropTypes.object),
+    givenProps: PropTypes.arrayOf(PropTypes.object),
     props_count: PropTypes.shape({
       received: PropTypes.number,
       given: PropTypes.number,
