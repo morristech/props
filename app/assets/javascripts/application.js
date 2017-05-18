5de3a9bca2948f5ba6c8e6b3edae9a645d4b3e97
Adam Nowak <adam.nowak@netguru.co>
2017-05-18 21:39:15 +0200

    get rid of coffee files and dependencies

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require js_stack/base/underscore/1.6.0
+//= require js_stack/base/jsroutes
+//= require js_stack/plugins/moment
+//= require select2
+//= require bootstrap
+
+//= require react_integration
+//= require react_bundle
