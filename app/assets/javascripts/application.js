ac57e1014823456af456cdfbf497e30d0fc06da9
Adam Nowak <adam.nowak@netguru.co>
2017-05-18 21:47:42 +0200

    remove js_stack

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-//= require js_stack/base/underscore/1.6.0
-//= require js_stack/base/jsroutes
-//= require js_stack/plugins/moment
 //= require select2
 //= require bootstrap
 
