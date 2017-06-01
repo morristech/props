77c2f0335f46a2ee8d9a6e61f034363b01c965b3
marek parafianowicz <marek.parafianowicz@netguru.co>
2017-06-01 22:49:55 +0200

    Add avatar column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517154118) do
+ActiveRecord::Schema.define(version: 20170601204451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20170517154118) do
     t.datetime "updated_at"
     t.boolean  "admin",       default: false
     t.datetime "archived_at"
+    t.string   "avatar"
   end
 
   add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
