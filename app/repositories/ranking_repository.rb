b5315c4bf153294dd38bf0a14083f7fc698b4229
marek parafianowicz <marek.parafianowicz@netguru.co>
2017-06-06 18:23:03 +0200

    Sort return by streak and small refactor

--- app/repositories/ranking_repository.rb
+++ app/repositories/ranking_repository.rb
@@ -15,9 +15,9 @@ class RankingRepository
   def team_activity
     team_activity_within
   end
-  
+
   def kudos_streak
-    kudos_streak_within
+    sorted_kudos_streak
   end
 
   private
@@ -31,13 +31,11 @@ class RankingRepository
   end
 
   def top_kudoers_within
-    users_kudos_count = users_with_kudos_count.sort_by { |_k, v| v }.reverse
-      .each_with_object({ top_kudoers: [] }) do |arr, hsh|
+    users_with_kudos_count.sort_by { |_k, v| v }.reverse
+      .each_with_object(top_kudoers: []) do |arr, hsh|
         hsh[:top_kudoers].push(
-          {
-            kudos_count: arr[1],
-            user: many_serialized_users[arr[0]],
-          }
+          kudos_count: arr[1],
+          user: many_serialized_users[arr[0]],
         )
       end
   end
@@ -47,22 +45,26 @@ class RankingRepository
   end
 
   def kudos_streak_within
-    users_with_kudos_count.each_with_object({ streaks: [] }) do |(user_id, _), hsh|
+    users_with_kudos_count.each_with_object([]) do |(user_id, _), arr|
       user = User.find(user_id)
       streak = count_user_streak(user)
-      hsh[:streaks].push(
-        {
-          streak: streak.max,
-          user: one_serialized_user(user)
-        }
+      arr.push(
+        streak: streak.max,
+        user: one_serialized_user(user),
       )
     end
   end
 
+  def sorted_kudos_streak
+    kudos_streak_within.sort { |x,y| y[:streak] <=> x[:streak] }
+  end
+
   def count_user_streak(user)
     streak = [1]
-    user.props.order(:created_at).each_cons(2) do |props_pair|
-      first_day, second_day = props_pair.first.created_at, props_pair.second.created_at
+    users_props = user.props.where(created_at: evaluate_time_range).order(:created_at)
+    users_props.each_cons(2) do |props_pair|
+      first_day = props_pair.first.created_at
+      second_day = props_pair.second.created_at
       if (first_day + 2.day) > second_day && (first_day + 1.day).day == second_day.day
         streak[-1] += 1
       elsif (first_day + 2.day) > second_day && first_day.day == second_day.day
@@ -88,24 +90,18 @@ class RankingRepository
 
   def evaluate_time_range
     @evaluated_time_range ||= case time_range
-    when "yearly"
-      (Time.zone.now - 1.year..Time.zone.now)
-    when "monthly"
-      (Time.zone.now - 1.month..Time.zone.now)
-    when "weekly"
-      (Time.zone.now - 1.week..Time.zone.now)
-    when "bi-weekly"
-      (Time.zone.now - 2.week..Time.zone.now)
-    when "all"
-      (Prop.order(:created_at).first.created_at..Time.zone.now)
-    else
-      raise "Wrong params"
+      when 'yearly' then (Time.zone.now - 1.year..Time.zone.now)
+      when 'monthly' then (Time.zone.now - 1.month..Time.zone.now)
+      when 'weekly' then (Time.zone.now - 1.week..Time.zone.now)
+      when 'bi-weekly' then (Time.zone.now - 2.week..Time.zone.now)
+      when 'all' then (Prop.order(:created_at).first.created_at..Time.zone.now)
+      else raise 'Wrong params'
     end
   end
 
   def count_time_interval
-    #TODO: change interval for "all"
-    return "month" if %w(yearly all).include?(time_range)
-    "day"
+    # TODO: change interval for "all"
+    return 'month' if %w(yearly all).include?(time_range)
+    'day'
   end
 end
