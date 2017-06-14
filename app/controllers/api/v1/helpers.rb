b82c404e6764f8c6641ab8ff3ad0e8fce99f7c1e
marek parafianowicz <marek.parafianowicz@netguru.co>
2017-06-14 11:04:04 +0200

    Validate user agent - use api helpers

--- app/controllers/api/v1/helpers.rb
+++ app/controllers/api/v1/helpers.rb
@@ -5,6 +5,8 @@ module Api
       include SessionHelpers
       include Pundit
 
+      PERMITED_BOT = 'Slackbot 1.0 (+https://api.slack.com/robots)'.freeze
+
       def authenticate_user!
         error!('401 Unauthorized', 401) if current_user.nil?
       end
@@ -17,6 +19,14 @@ module Api
       def token_valid?(token)
         return false if token.nil?
         EasyTokens::Token.exists?(value: token, deactivated_at: nil)
+      end
+
+      def require_user_agent!(user_agent)
+        error!('Wrong user agent', 401) unless permited_bot?(user_agent)
+      end
+
+      def permited_bot?(user_agent)
+        user_agent == PERMITED_BOT
       end
     end
   end
--- app/controllers/api/v1/slack_commands.rb
+++ app/controllers/api/v1/slack_commands.rb
@@ -4,20 +4,11 @@ module Api
       include Api::V1::Defaults
       helpers do
         include Api::V1::Helpers
-
-        PERMITED_BOT = 'Slackbot 1.0 (+https://api.slack.com/robots)'.freeze
-
-        def permited_bot?
-          request.user_agent.include?(PERMITED_BOT)
-        end
-
-        def wrong_user_agent_message
-          { text: I18n.t('slack_commands.kudos.errors.wrong_user_agent') }
-        end
       end
 
       before do
         require_api_auth!(params[:token])
+        require_user_agent!(request.user_agent)
       end
 
       namespace :slack_commands do
@@ -31,7 +22,6 @@ module Api
         end
 
         post :kudos do
-          return wrong_user_agent_message unless permited_bot?
           return unless params[:command] == '/kudos' || params[:command] == '/kudos_stg'
           ::SlackCommands::Kudos.new(params).call
         end
