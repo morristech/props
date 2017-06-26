89f664448e7cbb4a35278875025c135f15914ebf
marek parafianowicz <marek.parafianowicz@netguru.co>
2017-06-26 13:50:39 +0200

    Mock Slack response users list

--- spec/support/omniauth_helpers.rb
+++ spec/support/omniauth_helpers.rb
@@ -30,4 +30,33 @@ module OmniauthHelpers
       'credentials' => { 'token' => token },
     }
   end
+
+  def users_list_array(users_number: 1,
+                       email: 'aaa@bbb.cc',
+                       id: 'slack_id',
+                       name: 'mention',
+                       real_name: 'John Doe',
+                       big_avatar: 'slack.com/sample_avatar.png',
+                       is_admin: false,
+                       is_bot: false)
+    users_list_array = []
+    users_number.times do |i|
+      i = i.to_s
+      users_list_array <<
+        {
+          'id' => id + i,
+          'name' => name + i,
+          'deleted' => false,
+          'real_name' => real_name + i,
+          'profile' =>
+          {
+            'email' => i + email,
+            'image_512' => i + big_avatar,
+          },
+          'is_admin' => is_admin,
+          'is_bot' => is_bot,
+        }
+    end
+    users_list_array
+  end
 end
