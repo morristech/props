a2568a9f9c343fdf5d724fbe0e8844c0bf7615a4
marek parafianowicz <marek.parafianowicz@netguru.co>
2017-06-27 10:40:08 +0200

    Make internal model methods private

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,22 +26,12 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.update_with_slack_fetch(user_info)
-    slack_user(user_info).tap do |user|
-      user.update slack_fetch_attrs(user_info)
-    end
-  end
-
   def self.create_from_slack(member)
     create! slack_attrs(member)
   end
 
   def self.omniauth_user(auth)
     find_by(uid: auth['uid']) || find_by(email: auth['info']['email'])
-  end
-
-  def self.slack_user(user_info)
-    find_by(uid: user_info['id']) || find_by(email: user_info['profile']['email'])
   end
 
   def self.omniauth_attrs(auth)
@@ -52,17 +42,6 @@ class User < ActiveRecord::Base
       email: auth['info']['email'] || '',
       admin: auth['info']['is_admin'] || false,
       avatar: big_avatar_512(auth) || small_avatar_192(auth),
-    }
-  end
-
-  def self.slack_fetch_attrs(user_info)
-    {
-      provider: 'slack',
-      uid: user_info['id'],
-      name: name_from_user_info(user_info),
-      email: user_info['profile']['email'] || '',
-      admin: user_info['is_admin'] || false,
-      avatar: user_info['profile']['image_512'],
     }
   end
 
@@ -79,16 +58,37 @@ class User < ActiveRecord::Base
   class << self
     private
 
+    def update_with_slack_fetch(user_info)
+      slack_user(user_info).tap do |user|
+        user.update slack_fetch_attrs(user_info)
+      end
+    end
+
+    def slack_user(user_info)
+      find_by(uid: user_info['id']) || find_by(email: user_info['profile']['email'])
+    end
+
+    def slack_fetch_attrs(user_info)
+      {
+        provider: 'slack',
+        uid: user_info['id'],
+        name: name_from_user_info(user_info),
+        email: user_info['profile']['email'] || '',
+        admin: user_info['is_admin'] || false,
+        avatar: user_info['profile']['image_512'],
+      }
+    end
+
+    def name_from_user_info(user_info)
+      user_info['real_name'].blank? ? user_info['name'] : user_info['real_name']
+    end
+
     def big_avatar_512(auth)
       auth.dig('extra', 'user_info', 'user', 'profile', 'image_512')
     end
 
     def small_avatar_192(auth)
       auth['info']['image']
-    end
-
-    def name_from_user_info(user_info)
-      user_info['real_name'].blank? ? user_info['name'] : user_info['real_name']
     end
   end
 end
