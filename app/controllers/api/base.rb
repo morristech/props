a05b029642c262c840c11c171f9e32aed637a778
Grzegorz Kosowicz <grzegorz.kosowicz@netguru.com>
2019-04-30 15:40:55 +0200

    Use Yabeda gem to expose application metrics for Prometheus and Grafana

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,5 +1,11 @@
 module Api
   class Base < Grape::API
+    use Grape::Middleware::Globals
+    use GrapeLogging::Middleware::RequestLogger,
+        instrumentation_key: 'grape',
+        include: [GrapeLogging::Loggers::Response.new,
+                  GrapeLogging::Loggers::FilterParameters.new]
+
     content_type :json, 'application/json'
     default_format :json
 
---
+++ config/initializers/instrumentation.rb
@@ -0,0 +1,42 @@
+require 'yabeda'
+
+Yabeda.configure do
+  BUCKETS = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10].freeze
+
+  # DB queries metrics
+  group :db_query do
+    counter   :total, comment: 'A counter of the total number of DB queries.'
+    histogram :duration, unit: :seconds, buckets: BUCKETS, comment: 'A histogram of the DB query response latency.'
+  end
+
+  ActiveSupport::Notifications.subscribe('sql.active_record') do |*args|
+    event = ActiveSupport::Notifications::Event.new(*args)
+    labels = { name: event.payload[:name] }
+    db_query_total.increment(labels)
+    db_query_duration.measure(labels, (event.duration / 1000).round(3))
+  end
+
+  # Grape API metrics
+  group :grape_api do
+    counter   :requests_total, comment: 'A counter of the total number of HTTP requests grape_api processed.'
+    histogram :request_duration, unit: :seconds, buckets: BUCKETS,
+                                 comment: 'A histogram of the response latency.'
+    histogram :view_runtime, unit: :seconds, buckets: BUCKETS,
+                             comment: 'A histogram of the view rendering time.'
+    histogram :db_runtime, unit: :seconds, buckets: BUCKETS,
+                           comment: 'A histogram of the activerecord execution time.'
+  end
+
+  ActiveSupport::Notifications.subscribe('grape') do |*args|
+    event = ActiveSupport::Notifications::Event.new(*args)
+    labels = {
+      path: event.payload[:path],
+      status: event.payload[:status],
+      method: event.payload[:method].downcase,
+    }
+    grape_api_requests_total.increment(labels)
+    grape_api_request_duration.measure(labels, (event.payload[:time][:total].to_f / 1000).round(3))
+    grape_api_view_runtime.measure(labels, (event.payload[:time][:view].to_f / 1000).round(3))
+    grape_api_db_runtime.measure(labels, (event.payload[:time][:db].to_f / 1000).round(3))
+  end
+end
