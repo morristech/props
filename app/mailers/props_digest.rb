60553e27b7825ae93dd47937f4690fbad5b0fe74
Grzegorz Hauska <grzegorz.hauska@netguru.com>
2020-06-15 13:22:29 +0200

    use sendgrid api key

--- app/mailers/props_digest.rb
+++ app/mailers/props_digest.rb
@@ -2,7 +2,7 @@ class PropsDigest < ApplicationMailer
   def received(user, props)
     @props = props
     @user = user
-    if AppConfig.sendgrid_password.present?
+    if AppConfig.sendgrid_api_key.present?
       Yabeda.application.props_email_sent.increment(by: 1)
       mail(to: user.email, subject: 'Received props digest')
     end
