25dbc11f26fd6b033ee321e58286fd84be9020a2
Adrian Mikulski <adrian.mikulski@netguru.com>
2020-10-08 14:11:53 +0200

    crontab config cleanup

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -1,7 +1,3 @@
-require 'rake'
-Rails.app_class.load_tasks
-
-## copied from schedule.rb
 Crono.perform(CronTasks::EmailDigests::SendDailyJob).every 1.week, on: :monday, at: "17:00"
 Crono.perform(CronTasks::EmailDigests::SendDailyJob).every 1.week, on: :tuesday, at: "17:00"
 Crono.perform(CronTasks::EmailDigests::SendDailyJob).every 1.week, on: :wednesday, at: "17:00"
