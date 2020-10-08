40e945abad1b77dc6ec66cf244162cb83605a0d7
Adrian Mikulski <adrian.mikulski@netguru.com>
2020-10-08 14:17:54 +0200

    revert previous config

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -1,3 +1,26 @@
+module CronTasks
+  module EmailDigests
+    class SendDailyJob
+      def perform
+        ::EmailDigests::SendDaily.new.call
+      end
+    end
+
+    class WeeklyJob
+      def perform
+        ::EmailDigests::SendWeekly.new.call
+      end
+    end
+
+    class CreateSubscriptionsJob
+      def perform
+        ::EmailDigests::CreateSubscriptions.new.call
+      end
+    end
+  end
+end
+
+
 Crono.perform(CronTasks::EmailDigests::SendDailyJob).every 1.week, on: :monday, at: "17:00"
 Crono.perform(CronTasks::EmailDigests::SendDailyJob).every 1.week, on: :tuesday, at: "17:00"
 Crono.perform(CronTasks::EmailDigests::SendDailyJob).every 1.week, on: :wednesday, at: "17:00"
